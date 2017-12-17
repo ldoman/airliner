f946130ca712953454857474d12c6d2f53076746
cnalley <cnalley@windhoverlabs.com>
2017-12-17 21:25:04 +0000

    Fixed libjpeg linking. Improved VC_Custom_Utils.

--- config/aero/default/target/apps/vc/src/vc_dev_utils/vc_dev_utils.c
+++ config/aero/default/target/apps/vc/src/vc_dev_utils/vc_dev_utils.c
@@ -31,92 +31,123 @@
 *
 *****************************************************************************/
 
-//#include <opencv2/opencv.hpp>
-#include "vc_dev_utils.h"
-
-#include <inttypes.h>
+/************************************************************************
+** Includes
+*************************************************************************/
+/*
+ * Include file for users of JPEG library.
+ * You will need to have included system headers that define at least
+ * the typedefs FILE and size_t before you can include jpeglib.h.
+ * (stdio.h is sufficient on ANSI-conforming systems.)
+ * You may also wish to include "jerror.h".
+ */
 #include <stdio.h>
-
 #include "string.h"
-
+#include <inttypes.h>
 #include <jpeglib.h>
+#include <setjmp.h>
+
+#include "vc_platform_cfg.h"
+
+/************************************************************************
+** Local Defines
+*************************************************************************/
+#define VC_UTILS_JPEG_QUALITY       (100)
+#define VC_UTILS_IMAGE_WIDTH        VC_FRAME_WIDTH
+#define VC_UTILS_IMAGE_HEIGHT       VC_FRAME_HEIGHT
+#define VC_UTILS_COLOR_COMP_PER_PIX (3)
+/* libJPEG expects YUV 3bytes, 24bit */
+#define VC_UTILS_COLOR_SPACE        JCS_YCbCr
+#define VC_UTILS_BUFFER_SIZE        VC_MAX_BUFFER_SIZE 
 
 
-//int VC_Custom_YUVY2JPG(const void *Input, void *Output, int *SizeOut, const int Width, const int Height)
-//{
-    //bool returnBool = false;
-    //int returnCode = -1;
-    //int params[2] = {0};
-    ///* TODO */
-    //cv::vector<uchar> outputBuffer;
-    ///* TODO */
-    //cv::Mat original(Width, Height, CV_8UC2, (uchar*)Input);
-    //cv::Mat imgbgr;
+/************************************************************************
+** Structure Declarations
+*************************************************************************/
+/* TODO setup memory pool. The memory manager back end needs to be 
+ * modified to not use malloc and free see jmemnobs.c 
+ */
 
-    ///* Set quality */
-    //params[0] = CV_IMWRITE_JPEG_QUALITY;
-    //params[1] = 80;
-    
-    ///* Null check */
-    //if (0 == Input || 0 == Output || 0 == SizeOut)
-    //{
-        //goto end_of_function;
-    //}
+/* libjpeg will call exit() if the default error handler is used
+ * so we override the standard error routine. C's setjmp/longjmp is
+ * used to return control to the caller. See libjpeg example.c error
+ * handling for more info. 
+ */
 
-    ///* Convert from YUYV to BGR (opencv format) */
-    //cv::cvtColor(original, imgbgr, cv::COLOR_YUV2BGR_YUYV);
-    ///* Encode to jpeg */
-    //returnBool = cv::imencode(".jpg", imgbgr, outputBuffer, cv::vector<int>(params, params+1)); 
+struct jpegErrorManager
+{
+    /* "public" fields */
+    struct jpeg_error_mgr pub;
+    /* for return to caller */
+    jmp_buf setjmp_buffer;
+};
 
-    //if(true == returnBool)
-    //{
-        ///* Copy to the output buffer */
-        //*SizeOut = outputBuffer.size();
-        //memcpy(Output, outputBuffer.data(), (size_t)SizeOut);
-        //returnCode = 1;
-    //}
+typedef struct jpegErrorManager * my_error_ptr;
 
-//end_of_function:
 
-    //return returnCode;
-//}
+/************************************************************************
+** External Global Variables
+*************************************************************************/
+
+
+/************************************************************************
+** Function Prototypes
+*************************************************************************/
+void jpegErrorExit(j_common_ptr cinfo)
+{
+  /* cinfo->err actually points to a jpegErrorManager struct */
+  my_error_ptr myerr = (my_error_ptr) cinfo->err;
+  /* note : *(cinfo->err) is now equivalent to myerr->pub */
+  
+  /* Return control to the setjmp point */
+  longjmp(myerr->setjmp_buffer, 1);
+}
 
 
 int VC_Custom_Utils_YUVY2JPG(const char *Input, char *Output, int *SizeOut)
 {
     struct jpeg_compress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-    JSAMPROW row_ptr[1];
-    int row_stride;
-
-    uint8_t* outbuffer = NULL;
-    uint32_t outlen = 0;
+    struct jpegErrorManager jerr;
+    int row_stride = 0;
+    uint32_t outlen = VC_UTILS_BUFFER_SIZE;
+    int returnCode = 0;
 
     /* Null check */
     if(0 == Input || 0 == Output)
     {
-
+        returnCode = -1;
+        goto end_of_function;
     }
 
-    cinfo.err = jpeg_std_error(&jerr);
+    cinfo.err = jpeg_std_error(&jerr.pub);
+    jerr.pub.error_exit = jpegErrorExit;
+    
+    /* Establish the setjmp return context for my_error_exit to use. */
+    if (setjmp(jerr.setjmp_buffer)) 
+    {
+        /* If we get here, the JPEG code has signaled an error. */
+        returnCode = -2;
+        goto end_of_function;
+    }
+    
+    /* Initialize the JPEG compression object. */
     jpeg_create_compress(&cinfo);
-    /* TODO replace malloc */
-    jpeg_mem_dest(&cinfo, &outbuffer, &outlen);
 
-    // jrow is a libjpeg row of samples array of 1 row pointer
-    /* TODO width */
-    cinfo.image_width = 320 & -1;
-    /* TODO height */
-    cinfo.image_height = 240 & -1;
-    cinfo.input_components = 3;
-    cinfo.in_color_space = JCS_YCbCr; //libJPEG expects YUV 3bytes, 24bit
+    /* Set destination */
+    jpeg_mem_dest(&cinfo, (unsigned char **)&Output, (long unsigned int *)&outlen);
+
+    cinfo.image_width = VC_UTILS_IMAGE_WIDTH;
+    cinfo.image_height = VC_UTILS_IMAGE_HEIGHT;
+    cinfo.input_components = VC_UTILS_COLOR_COMP_PER_PIX;
+    cinfo.in_color_space = VC_UTILS_COLOR_SPACE;
 
     jpeg_set_defaults(&cinfo);
-    jpeg_set_quality(&cinfo, 100, TRUE);
+    /* Quantization table scaling */
+    jpeg_set_quality(&cinfo, VC_UTILS_JPEG_QUALITY, TRUE);
     jpeg_start_compress(&cinfo, TRUE);
 
-    /* TODO replace width */
-    char tmprowbuf[320 * 3];
+    /* JSAMPLEs per row in image_buffer */
+    char tmprowbuf[VC_UTILS_IMAGE_WIDTH * 3];
 
     JSAMPROW row_pointer[1];
     row_pointer[0] = &tmprowbuf[0];
@@ -124,21 +155,33 @@ int VC_Custom_Utils_YUVY2JPG(const char *Input, char *Output, int *SizeOut)
     while (cinfo.next_scanline < cinfo.image_height) 
     {
         unsigned i, j;
-        unsigned offset = cinfo.next_scanline * cinfo.image_width * 2; //offset to the correct row
-        for (i = 0, j = 0; i < cinfo.image_width * 2; i += 4, j += 6) { //input strides by 4 bytes, output strides by 6 (2 pixels)
-            tmprowbuf[j + 0] = Input[offset + i + 0]; // Y (unique to this pixel)
-            tmprowbuf[j + 1] = Input[offset + i + 1]; // U (shared between pixels)
-            tmprowbuf[j + 2] = Input[offset + i + 3]; // V (shared between pixels)
-            tmprowbuf[j + 3] = Input[offset + i + 2]; // Y (unique to this pixel)
-            tmprowbuf[j + 4] = Input[offset + i + 1]; // U (shared between pixels)
-            tmprowbuf[j + 5] = Input[offset + i + 3]; // V (shared between pixels)
+        /* offset to the correct row */
+        unsigned offset = cinfo.next_scanline * cinfo.image_width * 2;
+        /* input strides by 4 bytes, output strides by 6 (2 pixels) */
+        for (i = 0, j = 0; i < cinfo.image_width * 2; i += 4, j += 6) { 
+            /* Y (unique to this pixel) */
+            tmprowbuf[j + 0] = Input[offset + i + 0];
+            /* U (shared between pixels) */
+            tmprowbuf[j + 1] = Input[offset + i + 1];
+            /* V (shared between pixels) */
+            tmprowbuf[j + 2] = Input[offset + i + 3];
+            /* Y (unique to this pixel) */
+            tmprowbuf[j + 3] = Input[offset + i + 2];
+            /* U (shared between pixels) */
+            tmprowbuf[j + 4] = Input[offset + i + 1];
+            /* V (shared between pixels) */
+            tmprowbuf[j + 5] = Input[offset + i + 3];
         }
         jpeg_write_scanlines(&cinfo, row_pointer, 1);
     }
 
     jpeg_finish_compress(&cinfo);
+
     jpeg_destroy_compress(&cinfo);
 
     *SizeOut = outlen;
-    memcpy(Output, outbuffer, outlen);
+
+end_of_function:
+
+    return returnCode;
 }
--- config/aero/default/target/apps/vc/src/vc_dev_utils/vc_dev_utils.h
+++ config/aero/default/target/apps/vc/src/vc_dev_utils/vc_dev_utils.h
@@ -35,6 +35,40 @@
 extern "C" {
 #endif
 
+/************************************************************************/
+/** \brief Convert raw YUVY 4:2:2 buffer to JPEG.
+**
+**  \par Description
+**       From V4L2-PIX-FMT-YUYV documenation: In this format each four
+**       bytes is two pixels. Each four bytes is two Y's, a Cb and a Cr.
+**       Each Y goes to one of the pixels, and the Cb and Cr belong to 
+**       both pixels.
+**       Example V4L2_PIX_FMT_YUYV 4 × 4 pixel image:
+**
+**       Byte Order. Each cell is one byte.
+**       start + 0:  Y'00 Cb00 Y'01 Cr00 Y'02 Cb01 Y'03 Cr01
+**       start + 8:  Y'10 Cb10 Y'11 Cr10 Y'12 Cb11 Y'13 Cr11
+**       start + 16: Y'20 Cb20 Y'21 Cr20 Y'22 Cb21 Y'23 Cr21
+**       start + 24: Y'30 Cb30 Y'31 Cr30 Y'32 Cb31 Y'33 Cr31
+**
+**       Color Sample Location.
+**        0   1  2   3
+**      0 Y C Y  Y C Y
+**      1 Y C Y  Y C Y
+**      2 Y C Y  Y C Y
+**      3 Y C Y  Y C Y
+**
+**  \param [in]  Input    The pointer to the raw YUYV 4:2:2 buffer.
+**
+**  \param [out] Output   The pointer to the buffer to write the 
+**                        converted JPEG.
+**
+**  \param [out] SizeOut  The output size of the compressed JPEG.
+**
+**  \returns int 0 for success, -1 for null pointer, -2 for an internal
+**               libjpeg error. 
+**
+*************************************************************************/
 int VC_Custom_Utils_YUVY2JPG(const char *Input, char *Output, int *SizeOut);
 
 #ifdef __cplusplus
