da7591102ebf3170862f0626d352d6abba33b271
Luke Doman <ldoman@windhoverlabs.com>
2018-03-29 15:44:02 -0500

    Adding configurable TO ip to cmdr

--- config/shared/inc/msg_ids.h
+++ config/shared/inc/msg_ids.h
@@ -488,8 +488,8 @@
 #define EA_SEND_HK_MID                              0x1c2a
 #define EA_HK_TLM_MID                               0x0c2b
 #define EA_WAKEUP_MID                               0x1c2c
-#define EA_OUT_DATA_MID            					0x0c2d
-#define EA_PERFMON_MID            					0x1c2e
+#define EA_OUT_DATA_MID            		    0x0c2d
+#define EA_PERFMON_MID            		    0x1c2e
 
 /* VC                                                   */
 #define VC_HK_TLM_MID                               0x0c39
